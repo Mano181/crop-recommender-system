ff78c48ddf425771376f8bcff1debff51e23e784
Mano S <55449900+Mano181@users.noreply.github.com>
2022-05-10 09:05:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,26 +37,26 @@ def main():
 
 
  #   with col1:
-        st.subheader(" Find out the most suitable crop to grow in your farm 👨‍🌾")
-        N = st.number_input("Nitrogen", 1,10000)
-        P = st.number_input("Phosporus", 1,10000)
-        K = st.number_input("Potassium", 1,10000)
-        temp = st.number_input("Temperature",0.0,100000.0)
-        humidity = st.number_input("Humidity in %", 0.0,100000.0)
-        ph = st.number_input("Ph", 0.0,100000.0)
-        rainfall = st.number_input("Rainfall in mm",0.0,100000.0)
+    st.subheader(" Find out the most suitable crop to grow in your farm 👨‍🌾")
+    N = st.number_input("Nitrogen", 1,10000)
+    P = st.number_input("Phosporus", 1,10000)
+    K = st.number_input("Potassium", 1,10000)
+    temp = st.number_input("Temperature",0.0,100000.0)
+    humidity = st.number_input("Humidity in %", 0.0,100000.0)
+    ph = st.number_input("Ph", 0.0,100000.0)
+    rainfall = st.number_input("Rainfall in mm",0.0,100000.0)
 
-        feature_list = [N, P, K, temp, humidity, ph, rainfall]
-        single_pred = np.array(feature_list).reshape(1,-1)
+    feature_list = [N, P, K, temp, humidity, ph, rainfall]
+    single_pred = np.array(feature_list).reshape(1,-1)
         
-        if st.button('Predict'):
+   if st.button('Predict'):
 
             loaded_model = load_model('prediction.pkl')
             prediction = loaded_model.predict(single_pred)
-            col1.write('''
+            write('''
 		    ## Results 🔍 
 		    ''')
-            col1.success(f"{prediction.item().title()} are recommended crop for your farm.")
+            success(f"{prediction.item().title()} are recommended crop for your farm.")
       #code for html ☘️ 🌾 🌳 👨‍🌾  🍃
 
     
